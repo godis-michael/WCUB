b4c66b7c88abefb43ab3d75ea9f980be0ac64798
Mykhailo Hodis <godismichael1995@gmail.com>
2017-05-02 19:53:47 +0300

    - Робочі фільтри

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,22 +1,29 @@
+from django.contrib.postgres.fields import IntegerRangeField
+from django.contrib.postgres.validators import RangeMinValueValidator, RangeMaxValueValidator
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from datetime import datetime
 
 
 class Bancnote(models.Model):
-    validators_errors = {'year_length': 'Змініть рік на коректний',
-                         'par_length': 'Змініть номінал на коректний'}
+    validators_errors = {'par_length': 'Змініть номінал на коректний'}
 
-    type = models.CharField(max_length=11, default='Гривень')
+    HRYVNA = 'Гривня'
+    KARBOVANETS = 'Карбованець'
+
+    TYPE_CHOICES = (
+        (HRYVNA, 'Гривня'),
+        (KARBOVANETS, 'Карбованець')
+    )
+
+    type = models.CharField(max_length=11, choices=TYPE_CHOICES, default=HRYVNA)
     par = models.PositiveIntegerField(validators=[MinValueValidator(1, validators_errors['par_length']),
                                                   MaxValueValidator(1000000, validators_errors['par_length'])])
-    year_from = models.PositiveIntegerField(validators=[MinValueValidator(1917, validators_errors['year_length']),
-                                                        MaxValueValidator(2017, validators_errors['year_length'])])
-    year_to = models.PositiveIntegerField(validators=[MinValueValidator(1917, validators_errors['year_length']),
-                                                      MaxValueValidator(2017, validators_errors['year_length'])])
+    year = IntegerRangeField(validators=[RangeMinValueValidator(1917), RangeMaxValueValidator(datetime.now().year)])
     size = models.CharField(max_length=7)
     sign = models.CharField(max_length=20)
     desc = models.TextField(max_length=200)
     image = models.ImageField(upload_to='bons_images')
 
     def __str__(self):
-        return str(self.par) + ' ' + self.type + ' ' + str(self.year_from) + '-' + str(self.year_to)
+        return str(self.par) + ' ' + self.type + ' ' + str(self.year.lower) + '-' + str(self.year.upper)
--- catalogue/views.py
+++ catalogue/views.py
@@ -1,27 +1,38 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
+from django.shortcuts import render
 
-from catalogue.models import Bancnote
+from .models import Bancnote
+from .filters import BancnoteFilter
 
+
+# def search(request):
+#     user_list = User.objects.all()
+#     user_filter = UserFilter(request.GET, queryset=user_list)
+#     return render(request, 'search/user_list.html', {'filter': user_filter})
 
 def index(request):
     bons_list = Bancnote.objects.all().order_by('par')
-    page = request.GET.get('page', 1)
-    paginator = Paginator(bons_list, 20)
-    try:
-        bons = paginator.page(page)
-    except PageNotAnInteger:
-        bons = paginator.page(1)
-    except EmptyPage:
-        bons = paginator.page(paginator.num_pages)
+    bons_filter = BancnoteFilter(request.GET, queryset=bons_list)
+    # page = request.GET.get('page', 1)
+    # paginator = Paginator(bons_list, 20)
+    # try:
+    #     bons = paginator.page(page)
+    # except PageNotAnInteger:
+    #     bons = paginator.page(1)
+    # except EmptyPage:
+    #     bons = paginator.page(paginator.num_pages)
+    #
+    # index = bons.number - 1
+    # max_index = len(paginator.page_range)
+    # start_index = index - 5 if index >= 5 else 0
+    # end_index = index + 5 if index <= max_index - 5 else max_index
+    # page_range = paginator.page_range[start_index:end_index]
 
-    index = bons.number - 1
-    max_index = len(paginator.page_range)
-    start_index = index - 5 if index >= 5 else 0
-    end_index = index + 5 if index <= max_index - 5 else max_index
-    page_range = paginator.page_range[start_index:end_index]
-    return render(request, 'catalogue/index.html', {'bons': bons, 'page_range': page_range})
+    return render(request, 'catalogue/index.html', {'filter': bons_filter})
+# 'bons': bons,
+# 'page_range': page_range
 
 
 def image(request):
