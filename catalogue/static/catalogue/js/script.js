facceea3fe9620be3575fd5dab9f4c0df32c9c33
moriartyisntdead <artemivasyuk@mail.ru>
2017-05-16 12:26:30 +0300

    - Гарні кнопки, дороблено сайдбар

--- catalogue/static/catalogue/js/script.js
+++ catalogue/static/catalogue/js/script.js
@@ -1,31 +1,31 @@
 $(document).ready(function () {
-  var temp, denomination = [1,2,3,5,10,20,25,50,100,200,500,1000,2000,5000,10000,20000,
-      50000,100000,200000,500000,100000,200000,500000,1000000,2000000];
-  for(var i = 0; i < denomination.length; i++){
-      temp = document.createElement('option');
-      $(temp).attr('value', denomination[i]).html(denomination[i]);
-      $('#from').append(temp);
-  }
+    var temp, denomination = [1, 2, 3, 5, 10, 20, 25, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000,
+        50000, 100000, 200000, 500000, 100000, 200000, 500000, 1000000, 2000000];
+    for (var i = 0; i < denomination.length; i++) {
+        temp = document.createElement('option');
+        $(temp).attr('value', denomination[i]).html(denomination[i]);
+        $('#from').append(temp);
+    }
 
-  for(var i = denomination.length - 1; i >= 0; i--){
-      temp = document.createElement('option');
-      $(temp).attr('value', denomination[i]).html(denomination[i]);
-      $('#to').append(temp);
-  }
+    for (var i = denomination.length - 1; i >= 0; i--) {
+        temp = document.createElement('option');
+        $(temp).attr('value', denomination[i]).html(denomination[i]);
+        $('#to').append(temp);
+    }
 
-  $('#from, #to').change(function () {
-      if(parseInt($('#from').val()) > parseInt($('#to').val())){
-          temp = $('#to').val();
-          $('#to').val($('#from').val());
-          $('#from').val(temp);
-      }
-  });
+    $('#from, #to').change(function () {
+        if (parseInt($('#from').val()) > parseInt($('#to').val())) {
+            temp = $('#to').val();
+            $('#to').val($('#from').val());
+            $('#from').val(temp);
+        }
+    });
 
-  $('.toggle-menu').click(function () {
-      if($(this.lastChild).hasClass('fa-chevron-up')){
-          $(this.lastChild).removeClass('fa-chevron-up').addClass('fa-chevron-down');
-      } else $(this.lastChild).removeClass('fa-chevron-down').addClass('fa-chevron-up');
-  });
+    $('.toggle-menu').click(function () {
+        if ($(this.lastChild).hasClass('fa-chevron-up')) {
+            $(this.lastChild).removeClass('fa-chevron-up').addClass('fa-chevron-down');
+        } else $(this.lastChild).removeClass('fa-chevron-down').addClass('fa-chevron-up');
+    });
 
   // var body = document.body,
   //     html = document.documentElement,
@@ -36,23 +36,23 @@ $(document).ready(function () {
   //     $('footer').css('position', 'absolute');
   //   } else $('footer').css('position', 'relative');
 
-  $('#firstPer, #secPer').change(function () {
+    $('#firstPer, #secPer').change(function () {
         var val1 = parseInt($('#firstPer').val()),
             val2 = parseInt($('#secPer').val()),
             rs = $('.range-slider').children();
-        if(val1 > 2017) val1 = 2017;
-        if(val2 > 2017) val2 = 2017;
-        if(val1 < 1917) val1 = 1917;
-        if(val2 < 1917) val2 = 1917;
+        if (val1 > 2017) val1 = 2017;
+        if (val2 > 2017) val2 = 2017;
+        if (val1 < 1917) val1 = 1917;
+        if (val2 < 1917) val2 = 1917;
         // console.log('val1 = ' + val1,'val2 = ' + val2);
         // console.log('rs = ' + rs[0].value);
-        if(val1 > val2){
+        if (val1 > val2) {
             var temp = val2;
             val2 = val1;
             val1 = temp;
-        } else if(val1 === val2){
-            if(val1 === 2017) val1 -= 1;
-            else if(val1 === 1917) val2 += 1;
+        } else if (val1 === val2) {
+            if (val1 === 2017) val1 -= 1;
+            else if (val1 === 1917) val2 += 1;
             else val1 += 1;
         }
 
@@ -71,31 +71,35 @@ function showcontent(id) {
 }
 
 /*        slider           */
-function getVals(){
-  // Get slider values
-  var parent = this.parentNode;
-  var slides = parent.getElementsByTagName("input");
-    var slide1 = parseFloat( slides[0].value );
-    var slide2 = parseFloat( slides[1].value );
-  // Neither slider will clip the other, so make sure we determine which is larger
-  if( slide1 > slide2 ){ var tmp = slide2; slide2 = slide1; slide1 = tmp; }
-  console.log('val1 = ' + slide1,'val2 = ' + slide2);
-  $('#id_year_0').attr('value', slide1);
-  $('#id_year_1').attr('value', slide2);
+function getVals() {
+    // Get slider values
+    var parent = this.parentNode;
+    var slides = parent.getElementsByTagName("input");
+    var slide1 = parseFloat(slides[0].value);
+    var slide2 = parseFloat(slides[1].value);
+    // Neither slider will clip the other, so make sure we determine which is larger
+    if (slide1 > slide2) {
+        var tmp = slide2;
+        slide2 = slide1;
+        slide1 = tmp;
+    }
+    console.log('val1 = ' + slide1, 'val2 = ' + slide2);
+    $('#id_year_0').attr('value', slide1);
+    $('#id_year_1').attr('value', slide2);
 }
 
-window.onload = function(){
-  // Initialize Sliders
-  var sliderSections = document.getElementsByClassName("range-slider");
-      for( var x = 0; x < sliderSections.length; x++ ){
+window.onload = function () {
+    // Initialize Sliders
+    var sliderSections = document.getElementsByClassName("range-slider");
+    for (var x = 0; x < sliderSections.length; x++) {
         var sliders = sliderSections[x].getElementsByTagName("input");
-        for( var y = 0; y < sliders.length; y++ ){
-          if( sliders[y].type ==="range" ){
-            sliders[y].oninput = getVals;
-            // Manually trigger event first time to display values
-            sliders[y].oninput();
-          }
+        for (var y = 0; y < sliders.length; y++) {
+            if (sliders[y].type === "range") {
+                sliders[y].oninput = getVals;
+                // Manually trigger event first time to display values
+                sliders[y].oninput();
+            }
         }
-      }
+    }
 };
 /*         more slider      */
